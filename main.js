960648be3504598b67fbdab36db7f320f160a5b8
suchakree <chantanayingyong.s@gmail.com>
2025-07-21 17:07:57 +0700

    afternoon checkpoint

--- main.js
+++ main.js
@@ -22,13 +22,38 @@ class Field {
 		this.field[this.posX][this.posY] = pathCharacter;
 	}
 
+	//สร้าง method มาแจ้งว่าผู้เล่นตกหลุมตกขอบไหม หรือเดินเจอหมวกรึเปล่า
+	//เลือกใช้ while loop เพราะไม่รู้จำนวนที่แน่นอนในการเลือกเดินของผู้เล่น
+	startGame(){
+		let stayAlive = true; //ตั้งค่าตัวแปรให้เกมยังสามารถเล่น(ลูป)ได้อยู่ ตราบใดที่ผู้เล่นไม่เอาตัวละคร(pathCharacter) ไปตาย
+		while (stayAlive) {
+			this.print(); //ใช้ method print ว่าตัวละครเดินอยู่ตรงไหนแล้ว
+			this.askDirection(); //ใช้ method askDirection เพื่อถามผู้เล่นว่าจะไปไส
+			if (!this.insideMap()) {
+				console.log("You're dead because going off the map."); //เดินออกแมพ ทำแบบกดเดินออกแล้วตายไว้ก่อน
+				stayAlive= false; //ถ้าทันเดี๋ยวกลับมาทำต่อว่าให้เตือนหลายครั้งก่อนตาย "You're going off the map. This is the first time, don't do the second time."
+				break;
+			} else if (this.inHole()) {
+				console.log("REST IN PEACE"); //ตกหลุมตาย
+				stayAlive = false;
+				break;
+			} else if (this.foundHat()) {
+				console.log("GG WELL PLAYED"); //จบเกม เจอหมวก
+				stayAlive=false; //ตั้งชื่อไม่เหมาะเท่าไหร่ชนะได้หมวก แต่ stayAlive = false ถ้าทันจะมาเปลี่ยน
+				break;
+			}
+			this.field[this.posY][this.posX] = pathCharacter; //ไม่เข้าเงื่อนไข while loop ก็เดินต่อ
+		}
+	}
+
+	// Note! method เหมือน functionc แต่ไม่มีอิสระเท่า เน้นทำงานภายใน class
 	// สร้าง method เพื่อถามผู้เล่นว่าต้องการเดินไปทางไหน
 	askDirection() {
 		const direction = prompt("choose your way bro!").toLowerCase(); //ต้องใส่ method .lowercase ด้วย เพราะผู้เล่นอาจใช้พิมเล็กหรือพิมใหญ่
 		//เลือกใช้ wasd ง่ายดี เหมือนเล่นเกม
 		switch (direction) {
 			case "w":
-				this.posY -= 1; //เดินขึ้น
+				this.posY -= 1; //เดินขึ้น โดยกำหนดให้เดินทีละ 1 ตำแหน่งต่อครั้งเท่านั้น
 				break; //ถ้าเงื่อนไขตรงก็หยุดที่ case นั้นๆ
 			case "a":
 				this.posX -= 1; //เดินซ้าย
@@ -46,11 +71,48 @@ class Field {
 		}
 	}
 
-	//สร้าง method มาตรวจว่าผู้เล่นเดินไปไหน ตกหลุมตกขอบไหม หรือเดินเจอหมวกรึเปล่า
-	
+	//สร้าง methodError สำหรับเช็คว่าตัวละครอยู่ที่ไหนในแมพ
+	inHole() {
+		return this.field[this.posY][this.posX] === hole; //เช็คจุดเดินปัจจุบันว่าตกหลุมไหม (true/false) ถ้า true > stay alive = false
+	}
+
+	insideMap() {
+		return (
+			this.posY >=0 && this.posY < this.field.length &&
+			this.posX >=0 && this.posX < this.field[0].length //ต้องใส่ 0
+		);
+	}
+
+	foundHat() {
+		return this.field[this.posY][this.posX] === hat; //เช็คจุดเดินปัจจุบันว่าเจอหมวกไหม
+	}
+	print() {
+    const displayString = this.field.map(row => {
+        return row.join('');
+      }).join('\n');
+    console.log(displayString);
+  }
+
+  static generateField(height, width, percentage = 0.1) {
+    const field = new Array(height).fill(0).map(el => new Array(width));
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const prob = Math.random();
+        field[y][x] = prob > percentage ? fieldCharacter : hole;
+      }
+    }
+    const hatSpot = { //สุ่มจุดเกิดหมวก ใช้ math.random
+      x: Math.floor(Math.random() * width),
+      y: Math.floor(Math.random() * height)
+    };
+    while (hatSpot.x === 0 && hatSpot.y === 0) { //เช็คให้หมวกไม่เกิดจุดเดียวกับจุด start (0,0) โดยให้สุ่มเกิดใหม่
+      hatSpot.x = Math.floor(Math.random() * width);
+      hatSpot.y = Math.floor(Math.random() * height);
+    }
+    field[hatSpot.y][hatSpot.x] = hat;
+    return field;
+  }
 }
 
-// Game Mode ON
-// Remark: Code example below should be deleted and use your own code.
-const newGame = new Field();
-newGame.print();
+const playGame = new Field(Field.generateField(5, 7, 0.1)); //กำหนดจำนวน row, column และอัตราสุ่มหลุม
+playGame.startGame();
